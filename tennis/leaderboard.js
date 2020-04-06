8abb5e28185e629bb182e0b1745795e0259dce9f
OliverW10 <34265013+OliverW10@users.noreply.github.com>
2020-04-07 09:20:36 +1000

    leaderboard test

--- tennis/leaderboard.js
+++ tennis/leaderboard.js
@@ -1,8 +1,8 @@
 var leaderboard = "butt head"
 var http = new XMLHttpRequest();
-if (http.readyState == 4 && http.status == 200) {
-    leaderboard = http.responseText;
-}
+// if (http.readyState == 4 && http.status == 200) {
+//     leaderboard = http.responseText;
+// }
 http.open("GET", "https://game.chocolatejade42.repl.co");
 http.withCredentials = false;
 http.setRequestHeader("Content-Type", "application/json");
