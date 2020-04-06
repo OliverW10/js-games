b1467265db7f78093fed1f260a1d8028c32887cd
OliverW10 <34265013+OliverW10@users.noreply.github.com>
2020-04-07 09:06:59 +1000

    set leaderboard

--- tennis/leaderboard.js
+++ tennis/leaderboard.js
@@ -1,8 +1,9 @@
-// const Http = new XMLHttpRequest();
-// const url='https://game.chocolatejade42.repl.co/';
-// Http.open("POST", url);
-// Http.send();
-
-// Http.onreadystatechange = (e) => {
-//  	console.log(Http.responseText)
-// }
+var leaderboard = "butt head"
+var http = new XMLHttpRequest();
+if (http.readyState == 4 && http.status == 200) {
+    leaderboard = http.responseText;
+}
+http.open("GET", "https://game.chocolatejade42.repl.co");
+http.withCredentials = false;
+http.setRequestHeader("Content-Type", "application/json");
+http.send();
